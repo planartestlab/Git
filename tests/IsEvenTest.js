c247a862011f990092d85cb94858931a94205502
planartestlab <mathieu.bonnet@planar.com>
2016-02-16 11:22:29 +0100

    Test
    none

---
+++ tests/IsEvenTest.js
@@ -0,0 +1,12 @@
+
+QUnit.test('isEven()', function() {
+    ok(isEven(0), 'Zero is an even number');
+    ok(isEven(2), 'So is two');
+    ok(isEven(-4), 'So is negative four');
+    ok(!isEven(1), 'One is not an even number');
+    ok(!isEven(-7), 'Neither is negative seven');
+	
+	// Fails
+    ok(isEven(3), 'Three is an even number');
+})
+
